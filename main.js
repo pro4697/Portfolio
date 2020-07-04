155862ff0e908043e276f7f0d11f7a9064d3b59f
Kichan Yoon <pro4697@naver.com>
2020-07-04 23:33:31 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar__color');
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', () => {
+  const link = event.target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const id = document.querySelector(link);
+  //scrollTo.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  window.scrollTo({
+    top: id.getBoundingClientRect().top + window.pageYOffset - 65,
+    behavior: 'smooth',
+  });
+});
